4232583283593d3e617440e0f97245a0e0cfbf7a
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-01-30 21:19:39 +0100

    ruby script on regular expressions is updated

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/env ruby
 # First line which is the shebang indicating the path to ruby interpreter
 # A regular expression that is matches a given pattern
+# puts prints to the standard output
+# the scan method is used to find all occurances specified pattern in the input
+# The argv retrieves the first commadline arguments passed to the script
 puts ARGV[0].scan(/hbt{2,5}n/).join
